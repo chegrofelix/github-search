e3d7df884aa3a5ba62781b613cc4320e08350e0a
chegrofelix <chegerofelix@gmail.com>
2021-02-12 22:54:46 +0300

    changed port

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.use(express.static(__dirname + "/dist/github-search"));
 app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname + "/dist/github-search/index.html"));
 });
-app.listen(process.env.PORT||8080);
+app.listen(process.env.PORT||3000);
